f7c8e821d268843aa3a45a2a7d793f47397ed317
Salman-sajib <sfsajib@gmail.com>
2024-12-08 14:01:38 +0600

    modified the animation

--- app/page.js
+++ app/page.js
@@ -4,7 +4,7 @@ import Skills from "@/components/Skills";
 export default function Home() {
   return (
     <div className="bg-zinc-950 text-slate-50">
-      <main className="container mx-auto px-4">
+      <main className="mx-auto px-4">
         <Hero />
         <Skills />
       </main>
--- components/Hero.js
+++ components/Hero.js
@@ -1,26 +1,50 @@
 "use client";
-import { motion } from "motion/react";
+import { clamp, motion } from "motion/react";
 
 const containerVariants = {
   hidden: { opacity: 0 },
   visible: {
     opacity: 1,
     transition: {
-      staggerChildren: 0.3,
+      staggerChildren: 0.4,
     },
   },
 };
 
 const headingVariants = {
   hidden: {
+    y: 300,
+  },
+  visible: {
+    y: 0,
+    transition: {
+      duration: 0.5,
+    },
+  },
+};
+
+const groupVariants = {
+  hidden: {},
+  visible: {
+    transition: {
+      staggerChildren: 0.2,
+    },
+  },
+};
+
+const pathVariants = {
+  hidden: {
     opacity: 0,
-    y: 50,
+    y: -10,
   },
   visible: {
     opacity: 1,
     y: 0,
+    scale: 1.1,
     transition: {
-      duration: 0.5,
+      duration: 0.8,
+      repeat: Infinity,
+      repeatType: "reverse",
     },
   },
 };
@@ -31,31 +55,62 @@ function Hero() {
       variants={containerVariants}
       initial="hidden"
       animate="visible"
-      className="min-h-svh grid items-center justify-center"
+      className="relative min-h-svh grid items-center justify-center"
     >
       <motion.div
         variants={containerVariants}
         className="flex flex-col items-start gap-2"
       >
-        <motion.h1
-          variants={headingVariants}
-          className="bg-gradient-to-r from-blue-300 via-blue-100 to-green-300 text-transparent bg-clip-text text-4xl md:text-6xl lg:text-7xl font-bold capitalize max-w-max"
-        >
-          scroll down
-        </motion.h1>
-        <motion.h1
-          variants={headingVariants}
-          className="bg-gradient-to-r from-blue-300 via-blue-100 to-green-300 text-transparent bg-clip-text text-4xl md:text-6xl lg:text-7xl font-bold capitalize max-w-max"
-        >
-          to see the
-        </motion.h1>
-        <motion.h1
-          variants={headingVariants}
-          className="bg-gradient-to-r from-blue-300 via-blue-100 to-green-300 text-transparent bg-clip-text text-4xl h-[55px] md:h-[70px] lg:h-[82px] md:text-6xl lg:text-7xl font-bold capitalize max-w-max"
-        >
-          stagger animation
-        </motion.h1>
+        <div className="overflow-hidden">
+          <motion.h1
+            variants={headingVariants}
+            style={{
+              fontSize: "clamp(3rem, 15vw, 15rem)",
+              lineHeight: 1.2,
+            }}
+            className="bg-gradient-to-tr from-indigo-400 to bg-pink-400 text-transparent bg-clip-text font-bold capitalize"
+          >
+            stagger
+          </motion.h1>
+        </div>
+
+        <div className="overflow-hidden">
+          <motion.h1
+            variants={headingVariants}
+            style={{
+              fontSize: "clamp(3rem, 15vw, 15rem)",
+              lineHeight: 1.3,
+            }}
+            className="bg-gradient-to-tr from-indigo-400 to bg-pink-400 text-transparent bg-clip-text font-bold capitalize"
+          >
+            animation
+          </motion.h1>
+        </div>
       </motion.div>
+
+      <div className=" absolute bottom-[30px] left-1/2 -translate-x-1/2 flex flex-col items-center">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="24"
+          height="50"
+          viewBox="0 0 24 24"
+          fill="none"
+          stroke="currentColor"
+          strokeWidth="2"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          className="lucide lucide-chevrons-down"
+          initial="hidden"
+          animate="visible"
+        >
+          {/* Group for stagger */}
+          <motion.g variants={groupVariants}>
+            <motion.path d="m7 20 5 5 5-5" variants={pathVariants} />
+            <motion.path d="m7 13 5 5 5-5" variants={pathVariants} />
+            <motion.path d="m7 6 5 5 5-5" variants={pathVariants} />
+          </motion.g>
+        </svg>
+      </div>
     </motion.div>
   );
 }
